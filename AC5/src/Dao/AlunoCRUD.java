d763e9a9238ee578631cf438bdb5f3fcae4c8ec2
Borsoni <40211721+Borsoni99@users.noreply.github.com>
2023-11-05 18:03:14 -0300

    Add files via upload

---
+++ AC5/src/Dao/AlunoCRUD.java
@@ -0,0 +1,46 @@
+package Dao;
+import java.util.ArrayList;
+import java.util.List;
+
+import Entidades.Aluno;
+
+public class AlunoCRUD {
+
+    ArrayList<Aluno> alunos = new ArrayList<>();
+
+    public void adicionar(Aluno aluno) {
+        alunos.add(aluno);
+    }
+
+    public void remover(int matricula) {
+        for (Aluno aluno : alunos) {
+            if (aluno.getMatricula() == matricula) {
+                alunos.remove(aluno);
+                return;
+            }
+        }
+    }
+
+    public void atualizar(int matricula, String curso) {
+        for (Aluno aluno : alunos) {
+            if (aluno.getMatricula() == matricula) {
+                aluno.setCurso(curso);
+                return;
+            }
+        }
+    }
+
+    public Aluno listar(int matricula) {
+        for (Aluno aluno : alunos) {
+            if (aluno.getMatricula() == matricula) {
+                return aluno;
+            }
+        }
+        return null;
+    }
+
+    public List<Aluno> listarTodos() {
+        return alunos;
+    }
+
+}
